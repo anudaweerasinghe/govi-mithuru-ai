c023f442d43c691c5c6a7efdcab3bff783291745
Anuda Weerasinghe <anuda@live.com>
2024-06-12 17:31:08 +0530

    implements streaming and some system prompt updates

--- chat-mvp/app.py
+++ chat-mvp/app.py
@@ -5,7 +5,7 @@ import cohere
 import anthropic
 
 
-SI_SYSTEM_PROMPT = "ඔබ ශ්‍රී ලංකාවේ වී වගාව පිළිබඳ විශේෂඥයෙක්. ගොවියාගේ ප්‍රශ්නයට පිළිතුරු දීමට සපයා ඇති තොරතුරු භාවිතා කරන්න. සිංහලෙන් පමණක් පිළිතුරු දෙන්න."
+SI_SYSTEM_PROMPT = "ඔබ ශ්‍රී ලංකාවේ වී වගාව පිළිබඳ විශේෂඥයෙක්. ගොවියාගේ ප්‍රශ්නයට පිළිතුරු දීමට සපයා ඇති තොරතුරු භාවිතා කරන්න. කෙටි හා සරල පිළිතුරු දෙන්න - අවශ්‍ය නම් පාරිභෝගිකයාගෙන් තවත් ප්‍රශ්න අසන්න. සිංහලෙන් පමණක් පිළිතුරු දෙන්න."
 
 st.set_page_config(page_title="ගොවි-මිතු​රු AI", page_icon="👨🏾‍🌾", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("ගොවි-මිතු​රු AI 👨🏾‍🌾")
@@ -31,6 +31,10 @@ def init():
     
 pc_index, co, llm = init()
 
+def extract_from_stream(steam):
+    for event in stream:
+        yield event.delta.text
+
 if user_input := st.chat_input("ඔබේ ප්‍රශ්නය"):
   st.session_state.messages.append({"role": "user", "content": user_input})
 
@@ -41,29 +45,26 @@ for message in st.session_state.messages: # Display the prior chat messages
 # If last message is not from assistant, generate a new response
 if len(st.session_state.messages)>0 and st.session_state.messages[-1]["role"] == "user":
     with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
             
             query_emb = co.embed(texts=[user_input], input_type="search_query", model="embed-multilingual-v3.0").embeddings 
-            pc_results = pc_index.query(vector=query_emb, top_k=3, include_metadata=True, filter={"language": {"$eq": "si"}})
+            pc_results = pc_index.query(vector=query_emb, top_k=1, include_metadata=True, filter={"language": {"$eq": "si"}})
 
             context = "\n\n".join([result["metadata"]["content"] for result in pc_results["matches"]])
 
             messages_for_anthropic = st.session_state.messages[:-1]
 
 
-            message_with_context = f"ප්රශ්නය: {user_input}\n\nඅදාල තොරතුරු: {context}"
+            message_with_context = f"ප්රශ්නය: {user_input}\n\nඅදාල තොරතුරු: '{context}'"
             messages_for_anthropic.append({"role": "user", "content": message_with_context})
-            print(messages_for_anthropic)
-            response = llm.messages.create(
+
+            with llm.messages.stream(
                 model="claude-3-opus-20240229",
                 messages=messages_for_anthropic,
-                max_tokens=1024,
-                system=SI_SYSTEM_PROMPT
-            )
+                system=SI_SYSTEM_PROMPT,
+                max_tokens=4096,
+            ) as stream:
+                text_response = st.write_stream(stream.text_stream)
 
-            text_response = response.content[0].text
-
-            st.write(text_response)
             message = {"role": "assistant", "content": text_response}
             st.session_state.messages.append(message) # Add response to message history
 
