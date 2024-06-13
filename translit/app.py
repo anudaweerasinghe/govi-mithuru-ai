4ec9c15e6ed9b3994ce804fa4d8632a5f64d781d
Anuda Weerasinghe <anuda@live.com>
2024-06-13 13:22:54 +0530

    using custom flask endpoints instead of whole server

--- translit/app.py
+++ translit/app.py
@@ -1,2 +1,19 @@
-from ai4bharat.transliteration import xlit_server
-app, engine = xlit_server.get_app()
+from flask import Flask
+from ai4bharat.transliteration import XlitEngine
+
+app = Flask(__name__)
+
+sinhala_translit = XlitEngine(lang2use=["si"], beam_width=4, rescore=False, src_script_type = "en")
+tamil_translit = XlitEngine(lang2use=["ta"], beam_width=10, rescore=False, src_script_type = "en")
+
+@app.route('/si/<string:input_text>')
+def sinhala(input_text):
+    sinhala_text = sinhala_translit.translit_sentence(input_text)
+
+    return sinhala_text
+
+@app.route('/ta/<string:input_text>')
+def tamil(input_text):
+    tamil_text = tamil_translit.translit_sentence(input_text)
+
+    return tamil_text
