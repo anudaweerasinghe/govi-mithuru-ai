9a073630cd0a81da83b8ea5a9269b78be41e4536
Anuda Weerasinghe <anuda@live.com>
2024-06-14 00:40:11 +0530

    removes tamil translit service

--- translit/app.py
+++ translit/app.py
@@ -4,7 +4,7 @@ from ai4bharat.transliteration import XlitEngine
 app = Flask(__name__)
 
 sinhala_translit = XlitEngine(lang2use=["si"], beam_width=5, rescore=True, src_script_type = "en")
-tamil_translit = XlitEngine(lang2use=["ta"], beam_width=5, rescore=True, src_script_type = "en")
+# tamil_translit = XlitEngine(lang2use=["ta"], beam_width=5, rescore=True, src_script_type = "en")
 
 @app.route('/si/<string:input_text>')
 def sinhala(input_text):
@@ -12,8 +12,8 @@ def sinhala(input_text):
 
     return sinhala_text
 
-@app.route('/ta/<string:input_text>')
-def tamil(input_text):
-    tamil_text = tamil_translit.translit_sentence(input_text)
+# @app.route('/ta/<string:input_text>')
+# def tamil(input_text):
+#     tamil_text = tamil_translit.translit_sentence(input_text)
 
-    return tamil_text
+#     return tamil_text
