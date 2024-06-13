44370f9471c2c63af0e91c3e19000af9118abf41
Anuda Weerasinghe <anuda@live.com>
2024-06-14 01:11:43 +0530

    reverts to no rescoring

--- translit/app.py
+++ translit/app.py
@@ -3,8 +3,8 @@ from ai4bharat.transliteration import XlitEngine
 
 app = Flask(__name__)
 
-sinhala_translit = XlitEngine(lang2use=["si"], rescore=True, src_script_type = "en")
-# tamil_translit = XlitEngine(lang2use=["ta"], beam_width=5, rescore=True, src_script_type = "en")
+sinhala_translit = XlitEngine(lang2use=["si"], rescore=False, beam_width=10, src_script_type = "en")
+tamil_translit = XlitEngine(lang2use=["ta"], rescore=False, beam_width=10, src_script_type = "en")
 
 @app.route('/si/<string:input_text>')
 def sinhala(input_text):
@@ -12,8 +12,8 @@ def sinhala(input_text):
 
     return sinhala_text
 
-# @app.route('/ta/<string:input_text>')
-# def tamil(input_text):
-#     tamil_text = tamil_translit.translit_sentence(input_text)
+@app.route('/ta/<string:input_text>')
+def tamil(input_text):
+    tamil_text = tamil_translit.translit_sentence(input_text)
 
-#     return tamil_text
+    return tamil_text
